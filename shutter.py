6bce1cba5fd9cd4c7083e9f4c53867744b308ae1
defix121 <pugatim110@gmail.com>
2025-01-18 11:34:38 +0200

    Add files via upload

--- shutter.py
+++ shutter.py
@@ -6,19 +6,26 @@ mixer.music.load('music.ogg')
 mixer_music.play()
 fire_sound = mixer.Sound('shutt.ogg')
 
+
 font.init()
+font1 = font.Font(None, 80)
 font2 = font.Font(None, 36)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+
 
 img_back = "kosmo.png"
 img_hero = "rocket.png"
 img_enemy = "baxa.jpg"
+img_bullet = "nagets.png"
 
 clock = time.Clock()
 FPS = 60
 
+
 score = 0 
 lost = 0
-
+max_lost = 3
 
 class GameSprite(sprite.Sprite):    
     def __init__ (
@@ -42,6 +49,11 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
 
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
 
 class Player(GameSprite):
  
@@ -55,7 +67,10 @@ class Player(GameSprite):
  
     # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
     def fire(self):
-        pass
+        bullet = Bullet (
+            img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15  
+        )
+        bullets.add(bullet)
 
 class Enemy(GameSprite):
     def update(self):
@@ -83,7 +98,9 @@ for i in range(1, 6):
     monster = Enemy(img_enemy, randint(
     80, win_width - 80), -40, 80, 50, randint(1, 3))
     monsters.add(monster)
- 
+
+bullets = sprite.Group()
+
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
  
@@ -95,6 +112,10 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
  
     if not finish:
         # оновлюємо фон
@@ -109,6 +130,8 @@ while run:
         # рухи спрайтів
         ship.update()
         monsters.update()
+        bullets.update()
+        bullets.draw(window)
  
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
