3f88265c00b948dc2b178458e8ca0dd068796cdc
defix121 <pugatim110@gmail.com>
2025-01-25 11:25:35 +0200

    Add files via upload

--- shutter.py
+++ shutter.py
@@ -1,5 +1,6 @@
 from pygame import*
 from random import randint
+import time as pytime
 
 mixer.init()
 mixer.music.load('music.ogg')
@@ -24,8 +25,13 @@ FPS = 60
 
 
 score = 0 
+goal = 50
 lost = 0
 max_lost = 3
+life = 3
+bullet_count = 10
+reload_start_time = None
+
 
 class GameSprite(sprite.Sprite):    
     def __init__ (
@@ -95,8 +101,7 @@ ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
 
 monsters = sprite.Group()
 for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(
-    80, win_width - 80), -40, 80, 50, randint(1, 3))
+    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 3))
     monsters.add(monster)
 
 bullets = sprite.Group()
@@ -113,7 +118,8 @@ while run:
         if e.type == QUIT:
             run = False
         elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
+            if e.key == K_SPACE and bullet_count > 0 and reload_start_time is None:
+                bullet_count -= 1
                 fire_sound.play()
                 ship.fire()
  
@@ -136,7 +142,45 @@ while run:
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
         monsters.draw(window)
- 
+        bullets.draw(window)
+        if bullet_count == 0 and reload_start_time is None:
+            reload_start_time = pytime.time()
+
+        if reload_start_time:
+            if pytime.time() - reload_start_time > 3:
+                bullet_count = 10
+                reload_start_time = None
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for collide in collides:
+            score = score + 1 
+            monster = Enemy(
+                img_enemy,
+                randint(80, win_width - 80), -40, 80, 50, randint(1, 5)
+            )
+            monsters.add(monster)
+        if sprite.spritecollide(ship, monsters, True):
+            life -= 1
+            monster = Enemy(
+                img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5)
+            )
+            monsters.add(monster)
+        
+        text_life = font1.render(str(life), 1, (255, 0, 0))
+        window.blit(text_life, (650, 10))
+
+        if life == 0 or lost >= max_lost:
+            finish = True
+            mixer.music.stop()
+            window.blit(lose, (200, 200)) 
+
+        if score >= goal:
+            finish = True
+            mixer.music.stop()
+            window.blit(win, (200, 200))
+                        
+                
+
+            
     display.update()
     clock.tick(FPS)
 
